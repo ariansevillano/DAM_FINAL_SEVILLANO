026892b690ab757668613fdf158b6111e193e30b
ariansevillano <ariansevillano1@gmail.com>
2024-12-18 16:11:39 -0500

    Login

---
+++ app/src/main/java/com/example/dam_examen_final_sevillano/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dam_examen_final_sevillano;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main); // Cargar el layout primero
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Ahora puedes obtener la referencia del Spinner
+        Spinner spinnerOptions1 = findViewById(R.id.spinnerOptions1);
+
+        String[] options1 = {"DNI", "CARNET DE EXTRANJERÍA", "PASAPORTE"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, options1);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOptions1.setAdapter(adapter1);
+    }
+
+
+}
